a747ad06dec9d30e2547afcb819e3b37438d6b4e
fywest <fywest2109@hotmail.com>
2019-02-17 00:05:35 +0200

    1-3 c file add

---
+++ 3-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Please enter a value:");
+    scanf("%d",&a);
+    printf("a=%d\n",a);
+    printf("Please enter b value:");
+    scanf("%d",&b);
+    printf("b=%d\n",b);
+    c = a + b;
+    printf("c=%d\n",c);
+    return 0;
+}
